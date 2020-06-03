92f7fd477e12995bcf1489b610c8c1973131c8df
hiphop5782 <zdlsz@hanmail.net>
2020-06-03 22:33:38 +0900

    경로 수정

--- hRemote/test/com/hakademy/remote/test/Test13_ElevatorTest.java
+++ hRemote/test/com/hakademy/remote/test/Test13_ElevatorTest.java
@@ -6,7 +6,7 @@ public class Test13_ElevatorTest {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println(System.getProperty("java.home"));
 		String path = String.join("/", System.getProperty("java.home"), "bin", "java.exe");
-		String command = String.join(" ", "-jar", "\""+System.getProperty("user.dir")+"/h-remote.jar"+"\"");
+		String command = String.join(" ", "-jar", "\""+System.getProperty("user.dir")+"\\h-remote.jar"+"\"");
 		System.out.println("path = " + path);
 		System.out.println("command = " + command);
 		Elevator.executeAsAdmin(path, command);
