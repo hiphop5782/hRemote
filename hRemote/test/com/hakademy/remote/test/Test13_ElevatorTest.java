45fe1ce5bf90e179769690ad0658060b0cbdc8f8
hiphop5782 <zdlsz@hanmail.net>
2020-06-04 22:13:10 +0900

    elevator update

--- hRemote/test/com/hakademy/remote/test/Test13_ElevatorTest.java
+++ hRemote/test/com/hakademy/remote/test/Test13_ElevatorTest.java
@@ -5,10 +5,9 @@ import com.hakademy.remote.execute.Elevator;
 public class Test13_ElevatorTest {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println(System.getProperty("java.home"));
-		String path = String.join("\\", System.getProperty("java.home"), "bin", "java.exe");
+		String path = "\""+String.join("\\", System.getProperty("java.home"), "bin", "java.exe")+"\"";
 		String command = String.join(" ", "-jar", "\""+System.getProperty("user.dir")+"\\h-remote.jar"+"\"");
-		System.out.println("path = " + path);
-		System.out.println("command = " + command);
+		System.out.println(path + " " + command);
 		Elevator.executeAsAdmin(path, command);
 		Thread.currentThread().join();
 	}
