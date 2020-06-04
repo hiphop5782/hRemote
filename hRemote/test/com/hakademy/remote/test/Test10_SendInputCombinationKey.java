5deb34bea6259e389dac6be6706677b924c8ff4f
hiphop5782 <zdlsz@hanmail.net>
2020-06-04 22:13:54 +0900

    conflict

--- hRemote/test/com/hakademy/remote/test/Test10_SendInputCombinationKey.java
+++ hRemote/test/com/hakademy/remote/test/Test10_SendInputCombinationKey.java
@@ -6,10 +6,10 @@ import com.sun.jna.platform.win32.User32;
 public class Test10_SendInputCombinationKey {
 	public static void main(String[] args) throws InterruptedException {
 		KeyboardHook hook = KeyboardHook.getInstance();
-		hook.keyPress(KeyboardHook.CONTROL_LEFT);
-		hook.keyPress(KeyboardHook.ALT_LEFT);
+		hook.keyPress(KeyboardHook.VK_CONTROL_LEFT);
+		hook.keyPress(KeyboardHook.VK_ALT_LEFT);
 		Thread.sleep(3L);
-		hook.keyRelease(KeyboardHook.ALT_LEFT);
-		hook.keyRelease(KeyboardHook.CONTROL_LEFT);
+		hook.keyRelease(KeyboardHook.VK_ALT_LEFT);
+		hook.keyRelease(KeyboardHook.VK_CONTROL_LEFT);
 	}
 }
